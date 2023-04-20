272ebe4099744207b1d59231578bc963c0981b05
Kenichi Yasukata <kyasukata@outlook.com>
2023-04-20 13:44:30 +0900

    client mode

--- main.c
+++ main.c
@@ -109,26 +109,110 @@ static err_t low_level_output(struct netif *netif __attribute__((unused)), struc
 	return ERR_OK;
 }
 
-static err_t tcp_recv_handler(void *arg __attribute__((unused)), struct tcp_pcb *tpcb,
+static unsigned long io_stat[3] = { 0 };
+
+struct http_response {
+	int state;
+	long content_tot_len;
+	long content_recvd;
+	long cur;
+	char buf[1UL << 16];
+};
+
+static err_t tcp_recv_handler(void *arg, struct tcp_pcb *tpcb,
 			      struct pbuf *p, err_t err)
 {
-	char buf[4] = { 0 };
 	if (err != ERR_OK)
 		return err;
 	if (!p) {
 		tcp_close(tpcb);
 		return ERR_OK;
 	}
-	pbuf_copy_partial(p, buf, 3, 0);
-	if (!strncmp(buf, "GET", 3)) {
-		assert(tcp_sndbuf(tpcb) >= httpdatalen);
-		assert(tcp_write(tpcb, httpbuf, httpdatalen, TCP_WRITE_FLAG_COPY) == ERR_OK);
-		assert(tcp_output(tpcb) == ERR_OK);
+	io_stat[1] += p->tot_len;
+	if (!arg) { /* server mode */
+		char buf[4] = { 0 };
+		pbuf_copy_partial(p, buf, 3, 0);
+		if (!strncmp(buf, "GET", 3)) {
+			io_stat[0]++;
+			io_stat[2] += httpdatalen;
+			assert(tcp_sndbuf(tpcb) >= httpdatalen);
+			assert(tcp_write(tpcb, httpbuf, httpdatalen, TCP_WRITE_FLAG_COPY) == ERR_OK);
+			assert(tcp_output(tpcb) == ERR_OK);
+		}
+	} else { /* client mode */
+		struct http_response *r = (struct http_response *) arg;
+		assert(p->tot_len < (sizeof(r->buf) - r->cur));
+		pbuf_copy_partial(p, &r->buf[r->cur], p->tot_len, 0);
+		r->cur += p->tot_len;
+		r->buf[r->cur] = '\0';
+		switch (r->state) {
+		case 0:
+			{
+				long i;
+				for (i = 0; i < r->cur && r->state == 0; i++) {
+					if (r->buf[i] == 'C') {
+						if (r->cur - i > 15) {
+							if (!memcmp(&r->buf[i], "Content-Length:", 15)) {
+								long j;
+								for (j = 0; j < (r->cur - i - 15); j++) {
+									if (r->buf[i + 15 + j] == '\r') {
+										r->buf[i + 15 + j] = '\0';
+										assert(sscanf(&r->buf[i], "Content-Length: %ld", &r->content_tot_len) == 1);
+										r->buf[i + 15 + j] = '\r';
+										r->state = 1;
+										break;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			/* fall through */
+		case 1:
+			{
+				long i;
+				for (i = 0; i < r->cur - 4 && r->state == 1; i++) {
+					if (r->buf[i + 0] == '\r' && r->buf[i + 1] == '\n' && r->buf[i + 2] == '\r' && r->buf[i + 3] == '\n') {
+						r->cur -= i + 4;
+						r->content_recvd = 0;
+						r->state = 2;
+						break;
+					}
+				}
+			}
+			/* fall through */
+		case 2:
+			r->content_recvd += r->cur;
+			if (r->content_recvd == r->content_tot_len) {
+				io_stat[0]++;
+				io_stat[2] += 42;
+				assert(tcp_sndbuf(tpcb) >= 42);
+				assert(tcp_write(tpcb, "GET / HTTP/1.0\r\nConnection: Keep-Alive\r\n\r\n", 42, TCP_WRITE_FLAG_COPY) == ERR_OK);
+				assert(tcp_output(tpcb) == ERR_OK);
+				r->state = 0;
+			}
+			r->cur = 0;
+			break;
+		default:
+			assert(0);
+			break;
+		}
 	}
 	tcp_recved(tpcb, p->tot_len);
 	pbuf_free(p);
 	return ERR_OK;
 }
+
+static void tcp_destroy_handeler(unsigned char id __attribute__((unused)), void *data)
+{
+	if (data)
+		free(data);
+}
+
+static const struct tcp_ext_arg_callbacks tcp_ext_arg_cbs =  {
+	.destroy = tcp_destroy_handeler,
+};
 
 static err_t accept_handler(void *arg __attribute__((unused)), struct tcp_pcb *tpcb, err_t err)
 {
@@ -137,6 +221,9 @@ static err_t accept_handler(void *arg __attribute__((unused)), struct tcp_pcb *t
 
 	tcp_recv(tpcb, tcp_recv_handler);
 	tcp_setprio(tpcb, TCP_PRIO_MAX);
+
+	tcp_ext_arg_set_callbacks(tpcb, 0, &tcp_ext_arg_cbs);
+	tcp_ext_arg_set(tpcb, 0, NULL);
 
 	tcp_nagle_disable(tpcb);
 
@@ -148,14 +235,35 @@ static err_t accept_handler(void *arg __attribute__((unused)), struct tcp_pcb *t
 	return err;
 }
 
-static uint8_t _mac[6];
-static uint16_t _mtu;
+static err_t connected_handler(void *arg, struct tcp_pcb *tpcb, err_t err)
+{
+	if (err != ERR_OK)
+		return err;
+	if ((err = accept_handler(arg, tpcb, err)) != ERR_OK)
+		return err;
+
+	io_stat[2] += 42;
+	assert(tcp_sndbuf(tpcb) >= 42);
+	assert(tcp_write(tpcb, "GET / HTTP/1.0\r\nConnection: Keep-Alive\r\n\r\n", 42, TCP_WRITE_FLAG_COPY) == ERR_OK);
+	assert(tcp_output(tpcb) == ERR_OK);
+
+	return ERR_OK;
+}
 
 static err_t if_init(struct netif *netif)
 {
-	for (int i = 0; i < 6; i++)
-		netif->hwaddr[i] = _mac[i];
-	netif->mtu = _mtu;
+	{
+		struct rte_ether_addr ports_eth_addr;
+		assert(rte_eth_macaddr_get(0 /* port id */, &ports_eth_addr) >= 0);
+		for (int i = 0; i < 6; i++)
+			netif->hwaddr[i] = ports_eth_addr.addr_bytes[i];
+	}
+	{
+		uint16_t _mtu;
+		assert(rte_eth_dev_get_mtu(0 /* port id */, &_mtu) >= 0);
+		assert(_mtu <= PACKET_BUF_SIZE);
+		netif->mtu = _mtu;
+	}
 	netif->output = etharp_output;
 	netif->linkoutput = low_level_output;
 	netif->hwaddr_len = 6;
@@ -163,12 +271,13 @@ static err_t if_init(struct netif *netif)
 	return ERR_OK;
 }
 
-int main(int argc, char* const* argv)
+int main(int argc, char *const *argv)
 {
 	struct netif _netif = { 0 };
-	ip4_addr_t _addr, _mask, _gate;
+	ip4_addr_t _addr, _mask, _gate, _srv_ip;
 	size_t content_len = 1;
-	int server_port = 10000;
+	int server_port = 10000, num_conn = 1;
+	bool mode_server = true;
 
 	/* setting up dpdk */
 	{
@@ -197,16 +306,6 @@ int main(int argc, char* const* argv)
 
 			assert(rte_eth_dev_adjust_nb_rx_tx_desc(0 /* port id */, &nb_rxd, &nb_txd) >= 0);
 
-			{
-				struct rte_ether_addr ports_eth_addr;
-				assert(rte_eth_macaddr_get(0 /* port id */, &ports_eth_addr) >= 0);
-				for (int i = 0; i < 6; i++)
-					_mac[i] = ports_eth_addr.addr_bytes[i];
-			}
-
-			assert(rte_eth_dev_get_mtu(0 /* port id */, &_mtu) >= 0);
-			assert(_mtu <= PACKET_BUF_SIZE);
-
 			assert(rte_eth_rx_queue_setup(0 /* port id */, 0 /* queue */, nb_rxd,
 						rte_eth_dev_socket_id(0 /* port id */),
 						&dev_info.default_rxconf,
@@ -221,15 +320,17 @@ int main(int argc, char* const* argv)
 		}
 	}
 
-	/* parse other arg */
 	{
 		int ch;
 		bool _a = false, _g = false, _m = false;
-		while ((ch = getopt(argc, argv, "a:g:l:m:p:")) != -1) {
+		while ((ch = getopt(argc, argv, "a:c:g:l:m:p:s:")) != -1) {
 			switch (ch) {
 			case 'a':
 				inet_pton(AF_INET, optarg, &_addr);
 				_a = true;
+				break;
+			case 'c':
+				num_conn = atoi(optarg);
 				break;
 			case 'g':
 				inet_pton(AF_INET, optarg, &_gate);
@@ -244,6 +345,10 @@ int main(int argc, char* const* argv)
 				break;
 			case 'p':
 				server_port = atoi(optarg);
+				break;
+			case 's':
+				inet_pton(AF_INET, optarg, &_srv_ip);
+				mode_server = false;
 				break;
 			default:
 				assert(0);
@@ -262,9 +367,7 @@ int main(int argc, char* const* argv)
 		netif_set_up(&_netif);
 	}
 
-	/* main procedure */
-	{
-		struct tcp_pcb *tpcb, *_tpcb;
+	if (mode_server) { /* server mode */
 		{
 			size_t buflen = content_len + 256 /* for http hdr */;
 			char *content;
@@ -272,19 +375,42 @@ int main(int argc, char* const* argv)
 			assert((content = (char *) malloc(content_len + 1)) != NULL);
 			memset(content, 'A', content_len);
 			content[content_len] = '\0';
-			httpdatalen = snprintf(httpbuf, buflen, "HTTP/1.1 200 OK\r\nContent-Length: %lu\r\nConnection: keep-alive\r\n\r\n%s",
-					content_len, content);
+			httpdatalen = snprintf(httpbuf, buflen, "HTTP/1.1 200 OK\r\nContent-Length: %lu\r\nConnection: keep-alive\r\n\r\n%s", content_len, content);
 			free(content);
 			printf("http data length: %lu bytes\n", httpdatalen);
 		}
+		{
+			struct tcp_pcb *tpcb, *_tpcb;
+			assert((_tpcb = tcp_new()) != NULL);
+			assert(tcp_bind(_tpcb, IP_ADDR_ANY, server_port) == ERR_OK);
+			assert((tpcb = tcp_listen(_tpcb)) != NULL);
+			tcp_accept(tpcb, accept_handler);
+			tcp_ext_arg_set_callbacks(tpcb, 0, &tcp_ext_arg_cbs);
+			tcp_ext_arg_set(tpcb, 0, NULL);
+		}
+	} else { /* client mode */
+		int i;
+		printf("%d concurrent connection(s)\n", num_conn);
+		for (i = 0; i < num_conn; i++) {
+			struct tcp_pcb *tpcb;
+			assert((tpcb = tcp_new()) != NULL);
+			{
+				struct http_response *r;
+				assert((r = (struct http_response *) malloc(sizeof(struct http_response))) != NULL);
+				r->state = 0;
+				r->cur = 0;
+				tcp_arg(tpcb, (void *) r);
+				tcp_ext_arg_set(tpcb, 0, (void *) r);
+			}
+			assert(tcp_connect(tpcb, &_srv_ip, server_port, connected_handler) == ERR_OK);
+		}
+	}
 
-		assert((_tpcb = tcp_new()) != NULL);
-		assert(tcp_bind(_tpcb, IP_ADDR_ANY, server_port) == ERR_OK);
-		assert((tpcb = tcp_listen(_tpcb)) != NULL);
-		tcp_accept(tpcb, accept_handler);
+	printf("-- application has started --\n");
 
-		printf("-- application has started --\n");
-		/* primary loop */
+	/* primary loop */
+	{
+		unsigned long prev_ts = 0;
 		while (1) {
 			struct rte_mbuf *rx_mbufs[MAX_PKT_BURST];
 			unsigned short i, nb_rx = rte_eth_rx_burst(0 /* port id */, 0 /* queue id */, rx_mbufs, MAX_PKT_BURST);
@@ -300,6 +426,16 @@ int main(int argc, char* const* argv)
 			}
 			tx_flush();
 			sys_check_timeouts();
+			{
+				unsigned long now = ({ struct timespec ts; clock_gettime(CLOCK_REALTIME, &ts); (ts.tv_sec * 1000000000UL + ts.tv_nsec); });
+				if (now - prev_ts > 1000000000UL) {
+					printf("[%s]: %10lu Requests/sec  (rx %11lu bps, tx %11lu bbs)\n",
+							(mode_server ? "server" : "client"), io_stat[0], io_stat[1] * 8, io_stat[2] * 8);
+					memset(io_stat, 0, sizeof(io_stat));
+					prev_ts = now;
+				}
+
+			}
 		}
 	}
 
